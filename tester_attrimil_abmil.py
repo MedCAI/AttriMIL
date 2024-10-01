aa234638c4d10ed0a9dde78f8e6fda08462d44d8
MedCAI <144468112+MedCAI@users.noreply.github.com>
2024-10-01 12:16:30 +0800

    Update tester_attrimil_abmil.py

--- tester_attrimil_abmil.py
+++ tester_attrimil_abmil.py
@@ -14,7 +14,7 @@ from sklearn.preprocessing import label_binarize
 from sklearn.metrics import roc_auc_score, roc_curve
 from sklearn.metrics import auc as calc_auc
 
-from models.AttrMIL import AttrMIL
+from models.AttriMIL import AttriMIL
 from utils import *
 
 
